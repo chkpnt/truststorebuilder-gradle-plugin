6bd8b3b0bbb482624b7839918cdf5892c654e95f
Gregor Dschung <gregor@chkpnt.de>
2019-10-06 15:28:22 +0200

    Migrate from Github Pages Plugin to gradle-git-publish

--- scripts/publish-ghpages.sh
+++ scripts/publish-ghpages.sh
@@ -1,11 +1,19 @@
 #!/bin/bash -evu
 
-openssl aes-256-cbc -K $encrypted_287f75608f39_key -iv $encrypted_287f75608f39_iv -in .travis/github-deploy.key.enc -out .travis/github-deploy.key -d
-chmod 600 .travis/github-deploy.key
-eval $(ssh-agent -s)
-ssh-add .travis/github-deploy.key
+if [ "${TRAVIS:-false}" == true ]; then
+    openssl aes-256-cbc -K $encrypted_287f75608f39_key -iv $encrypted_287f75608f39_iv -in .travis/github-deploy.key.enc -out .travis/github-deploy.key -d
+    chmod 600 .travis/github-deploy.key
+    eval $(ssh-agent -s)
+    ssh-add .travis/github-deploy.key
+fi
 
-export GIT_AUTHOR_NAME=chkpnt CI
-export GIT_AUTHOR_EMAIL=chkpnt-ci@chkpnt.de
+TMP_USERNAME=$(git config --global user.name)
+TMP_EMAIL=$(git config --global user.email)
 
-./gradlew docs publishGhPages
+git config --global user.name "chkpnt CI"
+git config --global user.email chkpnt-ci@chkpnt.de
+
+./gradlew docs gitPublishPush
+
+git config --global user.name "$TMP_USERNAME"
+git config --global user.email "$TMP_EMAIL"
