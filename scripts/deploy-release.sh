17b734520958b720547dd37b17962bb3f25e911b
Gregor Dschung <gregor@chkpnt.de>
2021-07-25 16:27:21 +0200

    Fix wrong version number published to Gradle Plugin Portal

--- scripts/deploy-release.sh
+++ scripts/deploy-release.sh
@@ -2,5 +2,5 @@
 
 ./gradlew -Pgradle.publish.key=$GRADLE_PUBLISH_KEY \
           -Pgradle.publish.secret=$GRADLE_PUBLISH_SECRET \
-          -PisRelease=true \
+          -Prelease=true \
           publishPlugins
