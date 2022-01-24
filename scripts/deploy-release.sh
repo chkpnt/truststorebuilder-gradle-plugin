35c4c212df8c349e8195ce8d3852f42a2db450ee
Gregor Dschung <gregor@chkpnt.de>
2022-01-24 23:40:55 +0100

    Remove Travis-CI-pipeline

--- scripts/deploy-release.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -evu
-
-./gradlew -Pgradle.publish.key=$GRADLE_PUBLISH_KEY \
-          -Pgradle.publish.secret=$GRADLE_PUBLISH_SECRET \
-          -Prelease=true \
-          publishPlugins
